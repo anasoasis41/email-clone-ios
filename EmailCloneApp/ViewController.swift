38c050eabbbdf4ba8342a89c458996ac5a2d0325
Anas RIAHI <anas.riahi@smile-maroc.com>
2020-07-30 14:18:44 +0100

    Initial Commit

---
+++ EmailCloneApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmailCloneApp
+//
+//  Created by Anas Riahi on 7/30/20.
+//  Copyright © 2020 Anas Riahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
