203e3df074feef57cf1bb91ecc44eef714091f46
Anas RIAHI <anas.riahi@smile-maroc.com>
2020-08-12 20:49:12 +0100

    Group cell layout

---
+++ EmailCloneApp/View/groups/GroupsCell.swift
@@ -0,0 +1,22 @@
+//
+//  GroupsCell.swift
+//  EmailCloneApp
+//
+//  Created by Anas Riahi on 8/12/20.
+//  Copyright © 2020 Anas Riahi. All rights reserved.
+//
+
+import UIKit
+
+class GroupsCell: UITableViewCell {
+
+    
+    @IBOutlet weak var imageGroup: UIImageView!
+    @IBOutlet weak var nameGroupLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
